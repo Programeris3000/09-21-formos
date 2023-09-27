878efbf0ed7642a1103b60c0aa64a7c039551329
Programeris3000 <valdasbucius1@gmail.com>
2023-09-27 01:29:39 -0700

    asdad

--- script.js
+++ script.js
@@ -1,45 +1,140 @@
-
-const sumbitForms = [
+const preAddedStudents = [
   {
-    name: `John`,
-    surname: `Lenon`,
-  },{
-    age: 23,
-  },{
-    phone: `+3874541212`
-  },{
-    email: `dma@gmail.com`
-  },{
-    knowledge: 6
-  },{
-    group: `Feu-9`
-  },{
-    interest: ['Javascript', `Python`]
-  }
+
+  },
+  // {
+  //   name: 'Steeve',
+  //   surname: 'Travolta',
+  //   age: 23,
+  //   phone: `+8692144654`,
+  //   email: `vala@gmail.com`,
+  //   knowledge: 6,
+  //   group: `Feu-9`,
+  //   interest: ['Javascript', `Python`],
+  // },{
+  //   name: 'John',
+  //   surname: 'Piterson',
+  //   age: 29,
+  //   phone: `+800065944`,
+  //   email: `valatka@gmail.com`,
+  //   knowledge: 8,
+  //   group: `Feu-5`,
+  //   interest: [`Python`],
+  // },{
+  //   name: 'Mari',
+  //   surname: 'Perez',
+  //   age: 35,
+  //   phone: `+869214465452548`,
+  //   email: `perez@gmail.com`,
+  //   knowledge: 3,
+  //   group: `Feu-6`,
+  //   interest: ['Javascript', `Python`, `C++`],
+  // },{
+  //   name: 'Ona',
+  //   surname: 'Saugirdaitė',
+  //   age: 20,
+  //   phone: `+7592144654`,
+  //   email: `Ona@gmail.com`,
+  //   knowledge: 9,
+  //   group: `Feu-3`,
+  //   interest: ['Javascript', `Python`, `C++`, `Java`],
+  // },{
+  //   name: 'Jupiter',
+  //   surname: 'Star',
+  //   age: 45,
+  //   phone: `+37092144654`,
+  //   email: `star@gmail.com`,
+  //   knowledge: 1,
+  //   group: `Feu-2`,
+  //   interest: ['Javascript'],
+  // },  
 ]
+console.log(preAddedStudents)
+
+// testForm.addEventListener('input',(event)=>{
+//   let key = event.target.name
+//   let value = event.target.value
+//   localStorage.setItem(key, value)
+// })
+const nameInput = document.querySelector('#person-name')
+nameInput.value = localStorage.getItem('name-input')
+nameInput.addEventListener('input',()=>{
+  localStorage.setItem('name-input', nameInput.value)
+})
+
+const lastNameInput = document.querySelector('#person-last-name')
+  lastNameInput.value = localStorage.getItem('lastName-input')
+  lastNameInput.addEventListener('input',()=>{
+  localStorage.setItem('lastName-input', lastNameInput.value)
+})
+
+const ageInput = document.querySelector('#person-age')
+  ageInput.value = localStorage.getItem('age-input')
+  ageInput.addEventListener('input',()=>{
+  localStorage.setItem('age-input', ageInput.value)
+  })
+
+const phoneInput = document.querySelector('#personPhone')
+  phoneInput.value = localStorage.getItem('number-input')
+  phoneInput.addEventListener('input',()=>{
+    localStorage.setItem('number-input', phoneInput.value)
+  })
+
+const emailInput = document.querySelector('#personEmail')
+  emailInput.value = localStorage.getItem('email-input')
+  emailInput.addEventListener('input',()=>{
+  localStorage.setItem('email-input', emailInput.value)
+})
+
+const knowledgeInput = document.querySelector('#it_knowledge')
+  knowledgeInput.value = localStorage.getItem('knowledge-input')
+  knowledgeInput.addEventListener('input',()=>{
+  localStorage.setItem('knowledge-input', knowledgeInput.value)
+})
 
 
+const groupInput = testForm.groupNumber
+groupInput.value = localStorage.getItem('groupName')
+groupInput.forEach((group)=>{
+group.addEventListener('input', ()=>{
+  localStorage.setItem('groupName', group.value)
+})
+})
 
 
-const sumbitForms2 = [
-  {
-    name: `Petras`,
-    surname: `Gaugitis`,
-  },{
-    age: 36,
-  },{
-    phone: `+4445698`
-  },{
-    email: `dmaaasda@gmail.com`
-  },{
-    knowledge: 10
-  },{
-    group: `Feu-6`
-  },{
-    interest: [`Python`]
-  }
-]
+const checkedGroupElement = testForm.querySelector(`input[name = "groupNumber"][value="${localStorage.getItem('group-inputs')}"]`)
 
+if(checkedGroupElement){
+  checkedGroupElement.checked = true
+}
+
+const groupInputs = document.getElementsByName('groupNumber')
+groupInputs.forEach((group)=>{
+group.addEventListener('input',()=>{
+  localStorage.setItem('group-inputs', group.value)
+})
+})
+
+const localStorageInterests = JSON.parse(localStorage.getItem('interest-inputs'))
+
+if(localStorageInterests){
+  localStorageInterests.forEach(localStorageInterest =>{
+  const checkedInterestElement = testForm.querySelector(`[name="interestFor"][value="${localStorageInterest}"]`)
+  checkedInterestElement.checked = true
+  })
+}
+
+const interestInputs = document.getElementsByName('interestFor')
+interestInputs.forEach((interest=>{
+  interest.addEventListener('input',()=>{
+const checkedInterests = testForm.querySelectorAll('[name="interestFor"]:checked')
+const checkedInterestsValue = [...checkedInterests].map(oneInterest =>{
+  return oneInterest.value
+})
+
+    localStorage.setItem('interest-inputs', JSON.stringify(checkedInterestsValue))
+  })
+}))
 
 
 
@@ -49,7 +144,18 @@ const testForm = document.querySelector(`#testForm`) //paselektinu forma
 testForm.addEventListener('submit', (event)=>{
 event.preventDefault() //Anuliuoju defaultini narsykles perkrovima
 
-      //Paselektinu div elementa esanti html. Kiekviena kart paspaudus sukuriu div-item ir ji pridedu prie div elemento esancio html.
+      // preAddedStudents.push('name-input', nameInput.value)
+      // preAddedStudents.push('lastName-input', lastNameInput.value)
+      // preAddedStudents.push('age-input', ageInput.value)
+      // preAddedStudents.push('number-input', phoneInput.value)
+      // preAddedStudents.push('email-input', emailInput.value)
+      // preAddedStudents.push('knowledge-input', knowledgeInput.value)
+      // preAddedStudents.push('name-input', nameInput.value) 
+      // preAddedStudents.push('groupName')
+      console.log(preAddedStudents)
+
+
+
       let studentList = document.querySelector('#student-list')
       let divItem = document.createElement('div')
       divItem.classList = ('div-item')
@@ -96,10 +202,9 @@ event.preventDefault() //Anuliuoju defaultini narsykles perkrovima
           showData.classList.add('active')
         }
         })
-          
-      
     }
       dataShowButton()
+
 
       //Vardas
       const personNameValue = testForm.name.value
@@ -179,124 +284,267 @@ event.preventDefault() //Anuliuoju defaultini narsykles perkrovima
       languageUl.append(languageLi)
       }
 })
+
+localStorage.removeItem('languageName')
+localStorage.removeItem('interest-inputs')
+localStorage.removeItem('groupName')
+localStorage.removeItem('email-input')
+localStorage.removeItem('number-input')
+localStorage.removeItem('age-input')
+localStorage.removeItem('knowledge-input')
+localStorage.removeItem('name-input')
+localStorage.removeItem('lastName-input')
+testForm.reset()
+
   })
 
 }
 init()
 
-// preAddedContent(`Joe`, `Doe`, 9, `+37844124`, `gmail@gmail.com`, 9, 'feu-4', [`Javascript`, 'C++'])
-// preAddedContent(`John`, `Santana`, 4, `+378441454524`, `gm@gmail.com`, 5, 'feu-9', [`Javascript`])
-// preAddedContent(`Mike`, `Cooper`, 6, `+3784414573737`, `gomo@gmail.com`, 7, 'feu-4', [`Javascript`,`Python`,`Java`])
-// preAddedContent(`Virgis`, `Stakėnas`, 8, `+379878888`, `Kam@gmail.com`, 6, 'feu-5', [`Python`])
-// preAddedContent(`Leon`, `Somov`, 10, `+3734565`, `Popopapa@gmail.com`, 2, 'feu-10', [`C++`])
 
 
-function preAddedContent(name, surname, age, phone, email, knowledge, group, interest){
 
-  //Paselektinu div elementa esanti html. Kiekviena kart paspaudus sukuriu div-item ir ji pridedu prie div elemento esancio html.
-  let studentList = document.querySelector('#student-list')
-  let divItem = document.createElement('div')
-  divItem.classList = ('div-item')
-  studentList.prepend(divItem)
-  studentList.style.backgroundColor = 'lightBlue'
-  studentList.style.border = 'solid 2px'
+
+
+// preAddedContent()
+// function preAddedContent(){
+
+
+
+//   //Paselektinu div elementa esanti html. Kiekviena kart apsisukus ciklui sukuriu div-item ir ji pridedu prie div elemento esancio html.
+
+//   preAddedStudents.forEach(object=>{
+//     let studentList = document.querySelector('#student-list')
+//     let divItem = document.createElement('div')
+//     divItem.classList = ('div-item')
+//     studentList.prepend(divItem)
+//     studentList.style.backgroundColor = 'lightBlue'
+//     studentList.style.border = 'solid 2px'
+
+//     let name = object.name
+//     let surname = object.surname
+//     let age = object.age
+//     let phone = object.phone
+//     let email = object.email
+//     let knowledge = object.knowledge
+//     let group = object.group
+//     let interest = object.interest
+
+//   let deleteStudent = document.createElement('button')
+//   deleteStudent.id = 'delete-student-id'
+//   deleteStudent.textContent = `Delete student`
+//   divItem.prepend(deleteStudent)
   
-  let deleteStudent = document.createElement('button')
-  deleteStudent.id = 'delete-student-id'
-  deleteStudent.textContent = `Delete student`
-  divItem.prepend(deleteStudent)
+//   deleteStudent.addEventListener('click', function(){
+//     studentList.removeChild(divItem)
+//     let deleteMessage = document.createElement('span')
+//     deleteMessage.textContent = 'Student successfully deleted'
+//     deleteMessage.style.color = 'green'
+//     let deleteMessageWrapper = document.createElement('div')
+//     deleteMessageWrapper.append(deleteMessage)
+//     studentList.after(deleteMessageWrapper)
+//     //Ištrynimo funkcija.
+//     setTimeout(function() {
+//       deleteMessageWrapper.remove()
+//     }, 5000);
+//   })
   
-  deleteStudent.addEventListener('click', function(){
-    studentList.removeChild(divItem)
-    let deleteMessage = document.createElement('span')
-    deleteMessage.textContent = 'Student successfully deleted'
-    deleteMessage.style.color = 'green'
-    let deleteMessageWrapper = document.createElement('div')
-    deleteMessageWrapper.append(deleteMessage)
-    studentList.after(deleteMessageWrapper)
-    //Ištrynimo funkcija.
-    setTimeout(function() {
-      deleteMessageWrapper.remove()
-    }, 5000);
-  })
+//   // Mygtukas rodyti duomenis
+//   function dataShowButton(){
+//   let showData = document.createElement('button')
+//   showData.classList.add('defaultClass', 'active')
+//   showData.textContent = `Show person's data`
+//   divItem.appendChild(showData)
   
-  // Mygtukas rodyti duomenis
-  function dataShowButton(){
-  let showData = document.createElement('button')
-  showData.classList.add('defaultClass', 'active')
-  showData.textContent = `Show person's data`
-  divItem.appendChild(showData)
-  
-  showData.addEventListener('click',function(){
-    if(showData.classList.contains('active')){
-    showData.textContent = `Hide person's data`
-    personPhone.textContent = `Phone number: ${phone}`
-    personEmail.textContent = `Email address: ${email}`
-    showData.classList.remove('active')
-    }else{
-      showData.textContent = `Show person's data`
-      personPhone.textContent = `Phone number: ${maskedText}`
-      personEmail.textContent = `Email address: ${maskedText1}`
-      showData.classList.add('active')
-    }
-    })
+//   showData.addEventListener('click',function(){
+//     if(showData.classList.contains('active')){
+//     showData.textContent = `Hide person's data`
+//     personPhone.textContent = `Phone number: ${phone}`
+//     personEmail.textContent = `Email address: ${email}`
+//     showData.classList.remove('active')
+//     }else{
+//       showData.textContent = `Show person's data`
+//       personPhone.textContent = `Phone number: ${maskedText}`
+//       personEmail.textContent = `Email address: ${maskedText1}`
+//       showData.classList.add('active')
+//     }
+//     })
       
   
-  }
-  dataShowButton()
+//   }
+//   dataShowButton()
   
-  //Vardas
-  let personName = document.createElement('h2')
-  personName.textContent = `Name: ${name}`
-  divItem.append(personName)
+//   //Vardas
+//   let personName = document.createElement('h2')
+//   personName.textContent = `Name: ${name}`
+//   divItem.append(personName)
   
-  //Pavarde
-  const personLastName = document.createElement('h2')
-  personLastName.innerHTML = `Last name: ${surname}`
-  divItem.append(personLastName)
+//   //Pavarde
+//   const personLastName = document.createElement('h2')
+//   personLastName.innerHTML = `Last name: ${surname}`
+//   divItem.append(personLastName)
   
-  //Amzius
-  const personAge = document.createElement('h2')
-  personAge.textContent = `Age: ${age}`
-  divItem.append(personAge)
+//   //Amzius
+//   const personAge = document.createElement('h2')
+//   personAge.textContent = `Age: ${age}`
+//   divItem.append(personAge)
 
-  //Telefonas
-  let personPhone = document.createElement('h2')
-  let maskedText = '*'.repeat(phone.length);
-  personPhone.textContent = `Phone number: ${maskedText}`
-  divItem.append(personPhone)
+//   //Telefonas
+//   let personPhone = document.createElement('h2')
+//   let maskedText = '*'.repeat(phone.length);
+//   personPhone.textContent = `Phone number: ${maskedText}`
+//   divItem.append(personPhone)
   
-  //Elektroninis pastas
-  let personEmail = document.createElement('h2')
-  let maskedText1 = '*'.repeat(email.length);
-  personEmail.textContent = `Email address: ${maskedText1}`
-  divItem.append(personEmail)
+//   //Elektroninis pastas
+//   let personEmail = document.createElement('h2')
+//   let maskedText1 = '*'.repeat(email.length);
+//   personEmail.textContent = `Email address: ${maskedText1}`
+//   divItem.append(personEmail)
   
-  //It zinios
-  const personKnowledge = document.createElement('h2')
-  personKnowledge.textContent = `It Knowledge: ${knowledge}`
-  divItem.append(personKnowledge)
+//   //It zinios
+//   const personKnowledge = document.createElement('h2')
+//   personKnowledge.textContent = `It Knowledge: ${knowledge}`
+//   divItem.append(personKnowledge)
   
-  //Grupes pasirinkimas
-  const personGroup = document.createElement('h2')
-  personGroup.textContent = `Selected Group: ${group}`
-  divItem.append(personGroup)
+//   //Grupes pasirinkimas
+//   const personGroup = document.createElement('h2')
+//   personGroup.textContent = `Selected Group: ${group}`
+//   divItem.append(personGroup)
   
-  //Dominancios kalbos
-  const interestDiv = document.createElement('div')
-  const interestUlName = document.createElement('h2')
-  interestUlName.textContent = (`Selected Language('s)`)
-  interestDiv.prepend(interestUlName)
-  const interestUl = document.createElement('ul')
+//   //Dominancios kalbos
+//   const interestDiv = document.createElement('div')
+//   const interestUlName = document.createElement('h2')
+//   interestUlName.textContent = (`Selected Language('s)`)
+//   interestDiv.prepend(interestUlName)
+//   const interestUl = document.createElement('ul')
+//   interest.forEach((item)=>{
+//     divItem.append(interestDiv)
+//     interestDiv.append(interestUl)
+//     const interestFor = document.createElement('li')
+//     interestUl.append(interestFor)
+//     interestFor.textContent = item
+//   })
+//   })
+//   }
+
+// function fromLocalData(){
+
+//     //Paselektinu div elementa esanti html. Kiekviena kart apsisukus ciklui sukuriu div-item ir ji pridedu prie div elemento esancio html.
   
-  interest.forEach((item)=>{
-    divItem.append(interestDiv)
-    interestDiv.append(interestUl)
-    const interestFor = document.createElement('li')
-    interestUl.append(interestFor)
-    interestFor.textContent = item
-  })
-  }
+//     preAddedStudents.forEach(object=>{
+//       let studentList = document.querySelector('#student-list')
+//       let divItem = document.createElement('div')
+//       divItem.classList = ('div-item')
+//       studentList.prepend(divItem)
+//       studentList.style.backgroundColor = 'lightBlue'
+//       studentList.style.border = 'solid 2px'
+  
+//       let name = nameInput.value
+//       let surname = lastNameInput.value
+//       let age = ageInput.value
+//       let phone = phoneInput.value
+//       let email = emailInput.value
+//       let knowledge = knowledgeInput.value
+//       let group = groupInput.value
+//       let interest = object.interest
+  
+//     let deleteStudent = document.createElement('button')
+//     deleteStudent.id = 'delete-student-id'
+//     deleteStudent.textContent = `Delete student`
+//     divItem.prepend(deleteStudent)
+    
+//     deleteStudent.addEventListener('click', function(){
+//       studentList.removeChild(divItem)
+//       let deleteMessage = document.createElement('span')
+//       deleteMessage.textContent = 'Student successfully deleted'
+//       deleteMessage.style.color = 'green'
+//       let deleteMessageWrapper = document.createElement('div')
+//       deleteMessageWrapper.append(deleteMessage)
+//       studentList.after(deleteMessageWrapper)
+//       //Ištrynimo funkcija.
+//       setTimeout(function() {
+//         deleteMessageWrapper.remove()
+//       }, 5000);
+//     })
+    
+//     // Mygtukas rodyti duomenis
+//     function dataShowButton(){
+//     let showData = document.createElement('button')
+//     showData.classList.add('defaultClass', 'active')
+//     showData.textContent = `Show person's data`
+//     divItem.appendChild(showData)
+    
+//     showData.addEventListener('click',function(){
+//       if(showData.classList.contains('active')){
+//       showData.textContent = `Hide person's data`
+//       personPhone.textContent = `Phone number: ${phone}`
+//       personEmail.textContent = `Email address: ${email}`
+//       showData.classList.remove('active')
+//       }else{
+//         showData.textContent = `Show person's data`
+//         personPhone.textContent = `Phone number: ${maskedText}`
+//         personEmail.textContent = `Email address: ${maskedText1}`
+//         showData.classList.add('active')
+//       }
+//       })
+        
+    
+//     }
+//     dataShowButton()
+    
+//     //Vardas
+//     let personName = document.createElement('h2')
+//     personName.textContent = `Name: ${name}`
+//     divItem.append(personName)
+    
+//     //Pavarde
+//     const personLastName = document.createElement('h2')
+//     personLastName.innerHTML = `Last name: ${surname}`
+//     divItem.append(personLastName)
+    
+//     //Amzius
+//     const personAge = document.createElement('h2')
+//     personAge.textContent = `Age: ${age}`
+//     divItem.append(personAge)
+  
+//     //Telefonas
+//     let personPhone = document.createElement('h2')
+//     let maskedText = '*'.repeat(phone.length);
+//     personPhone.textContent = `Phone number: ${maskedText}`
+//     divItem.append(personPhone)
+    
+//     //Elektroninis pastas
+//     let personEmail = document.createElement('h2')
+//     let maskedText1 = '*'.repeat(email.length);
+//     personEmail.textContent = `Email address: ${maskedText1}`
+//     divItem.append(personEmail)
+    
+//     //It zinios
+//     const personKnowledge = document.createElement('h2')
+//     personKnowledge.textContent = `It Knowledge: ${knowledge}`
+//     divItem.append(personKnowledge)
+    
+//     //Grupes pasirinkimas
+//     const personGroup = document.createElement('h2')
+//     personGroup.textContent = `Selected Group: ${group}`
+//     divItem.append(personGroup)
+    
+//     //Dominancios kalbos
+//     const interestDiv = document.createElement('div')
+//     const interestUlName = document.createElement('h2')
+//     interestUlName.textContent = (`Selected Language('s)`)
+//     interestDiv.prepend(interestUlName)
+//     const interestUl = document.createElement('ul')
+//     interest.forEach((item)=>{
+//       divItem.append(interestDiv)
+//       interestDiv.append(interestUl)
+//       const interestFor = document.createElement('li')
+//       interestUl.append(interestFor)
+//       interestFor.textContent = item
+//     })
+//     })
+// }
+// fromLocalData()
 
 
 
